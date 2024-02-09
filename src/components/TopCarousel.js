cbbe57ed547f20c33229ff46d65434ef2d00729d
amarsharmamrj <amarsharmamrj@gmail.com>
2024-02-09 14:34:00 +0530

    images added

--- src/components/TopCarousel.js
+++ src/components/TopCarousel.js
@@ -1,12 +1,12 @@
 import { Grid } from "@mui/material";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from 'react-responsive-carousel';
-// import image1 from '../assests/carousel/carousel1.jpeg'
-import image1 from '../assests/carousel/1.jpeg'
+import image1 from '../assests/carousel/carousel1.jpeg'
+// import image1 from '../assests/carousel/carousel3.jpeg'
 import image2 from '../assests/carousel/carousel2.jpeg'
-// import image3 from '../assests/carousel/carousel3.jpeg'
+import image3 from '../assests/carousel/carousel3.jpeg'
 import image4 from '../assests/carousel/carousel4.jpeg'
-// import image5 from '../assests/carousel/carousel5.jpeg'
+import image5 from '../assests/carousel/carousel5.jpeg'
 // import image6 from '../assests/carousel/carousel6.jpeg'
 
 const TopCarousel = () => {
@@ -30,15 +30,15 @@ const TopCarousel = () => {
                         <div>
                             <img src={image2} alt="" />
                         </div>
-                        {/* <div>
+                        <div>
                             <img src={image3} alt="" />
-                        </div> */}
+                        </div>
                         <div>
                             <img src={image4} alt="" />
                         </div>
-                        {/* <div>
+                        <div>
                             <img src={image5} alt="" />
-                        </div> */}
+                        </div>
                         {/* <div>
                             <img src={image6} alt="" />
                         </div> */}
